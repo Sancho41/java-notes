71dccbc84e2d4874469c3b4a826c7d3e2ed64270
Gabriel Sancho <gabriel.sancho13@gmail.com>
2020-07-05 18:17:39 -0300

    feat: Adicionado DAO para register e getUserById

--- src/main/java/dev/gabrielsancho/notas/persistence/UserDAO.java
+++ src/main/java/dev/gabrielsancho/notas/persistence/UserDAO.java
@@ -1,6 +1,7 @@
 package dev.gabrielsancho.notas.persistence;
 
 import dev.gabrielsancho.notas.dtos.LoginDTO;
+import dev.gabrielsancho.notas.dtos.RegisterUserDTO;
 import dev.gabrielsancho.notas.logs.LoggerBuilder;
 import dev.gabrielsancho.notas.model.User;
 import dev.gabrielsancho.notas.utils.HibernateUtils;
@@ -10,7 +11,7 @@ import javax.persistence.Query;
 
 public class UserDAO {
 
-    public User autentica(LoginDTO loginDTO) throws Exception {
+    public User authenticate(LoginDTO loginDTO) throws Exception {
         EntityManager em = HibernateUtils.createEntityManager();
         try {
 
@@ -18,22 +19,64 @@ public class UserDAO {
 
             Query query = em.createQuery(jpql);
             query.setParameter("pEmail", loginDTO.getEmail());
-            User queryUser = (User)query.getSingleResult();
+            User queryUser = (User) query.getSingleResult();
 
             if (!queryUser.checkPassword(loginDTO.getPassword())) {
-                throw new Exception("Password does not matck");
+                throw new Exception("Password does not match");
             }
+            LoggerBuilder.INFO(
+                    String.format("Usuário %s de id %d autenticado", queryUser.getName(), queryUser.getId())).log();
 
             return queryUser;
-        }catch (Exception e){
+        } catch (Exception e) {
             em.getTransaction().rollback();
             LoggerBuilder.ERROR("Não foi possivel autenticar Usuário", e).log();
             throw (e);
-        }finally {
+        } finally {
             em.close();
         }
     }
 
 
+    public User register(RegisterUserDTO registerUserDTO) throws Exception{
+        EntityManager em = HibernateUtils.createEntityManager();
 
+
+        em.getTransaction().begin();
+
+        try {
+            User newUser = new User(registerUserDTO);
+            if (!registerUserDTO.passwordMatch()) {
+                throw new Exception("Password confirmation do not match");
+            }
+
+            em.persist(newUser);
+            em.getTransaction().commit();
+
+            LoggerBuilder.INFO(
+                    String.format("Usuário %s criado com id %d", newUser.getName(), newUser.getId())).log();
+            return newUser;
+
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            LoggerBuilder.ERROR("Não foi possível criar Usuário", e).log();
+            throw(e);
+        } finally {
+            em.close();
+        }
+    }
+
+    public User getUserById(Long id)  throws Exception{
+        EntityManager em = HibernateUtils.createEntityManager();
+
+        try {
+            return em.find(User.class, id);
+        } catch (Exception e) {
+            LoggerBuilder.ERROR(String.format("Não foi possível encontrar usuário de id %d", id), e).log();
+            em.getTransaction().rollback();
+            throw(e);
+        } finally {
+            em.close();
+        }
+    }
 }
