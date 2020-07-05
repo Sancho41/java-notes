eb9b3433ef4c0fde5eaacc15714dd5ce8a3afbdc
Márcio Alexandre <Freire Sindeaux>
2020-07-05 06:32:17 -0300

    refact: Ajuste em UserService

--- src/main/java/dev/gabrielsancho/notas/services/UserService.java
+++ src/main/java/dev/gabrielsancho/notas/services/UserService.java
@@ -1,40 +1,28 @@
 package dev.gabrielsancho.notas.services;
 
 import com.google.gson.Gson;
+import dev.gabrielsancho.notas.dtos.LoginDTO;
 import dev.gabrielsancho.notas.model.User;
+import dev.gabrielsancho.notas.persistence.UserDAO;
 import dev.gabrielsancho.notas.security.TokenSecurity;
+import dev.gabrielsancho.notas.utils.HibernateUtils;
 
 import javax.json.JsonObject;
-import javax.persistence.*;
+import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.HttpHeaders;
 import java.util.List;
 
 public class UserService {
 
-    private EntityManager createEntityManager(){
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("notas");
-        return emf.createEntityManager();
-    }
+    public final UserDAO dao = new UserDAO();
 
-    public User autentica(JsonObject userAtempt) throws Exception {
-
-        EntityManager em = createEntityManager();
-
-        String jpql = "select u from User u where u.email = :pEmail";
-
-        Query query = em.createQuery(jpql);
-        query.setParameter("pEmail", userAtempt.getString("email"));
-        User queryUser = (User)query.getSingleResult();
-
-        if (queryUser.checkPassword(userAtempt.getString("password"))) {
-            return queryUser;
-        }
-        throw new Exception("Login failed exception");
+    public User autentica(LoginDTO loginDTO) throws Exception {
+        return dao.autentica(loginDTO);
     }
 
     public User registra(JsonObject userJSON) throws Exception {
-        EntityManager em = createEntityManager();
+        EntityManager em = HibernateUtils.createEntityManager();
 
         String password = userJSON.getString("password");
         String password_confirmation = userJSON.getString("password_confirmation");
@@ -53,14 +41,16 @@ public class UserService {
         throw new Exception("Register failed exception");
     }
 
+
+
     public User getLoggedUser(HttpHeaders headers) throws Exception {
-        EntityManager em = createEntityManager();
+        EntityManager em = HibernateUtils.createEntityManager();
         List<String> id = headers.getRequestHeader("id");
         return em.find(User.class, Long.parseLong(id.get(0)));
     }
 
     public User getLoggedUser(HttpServletRequest servletRequest) throws Exception {
-        EntityManager em = createEntityManager();
+        EntityManager em = HibernateUtils.createEntityManager();
         String id = TokenSecurity.validateJwtToken(servletRequest.getHeader("authorization"));
         return em.find(User.class, Long.parseLong(id));
     }
